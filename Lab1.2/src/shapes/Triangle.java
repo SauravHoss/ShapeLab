5e3ca9dde112ef87c6098f637fe5413f9e450f69
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-11-15 09:24:46 -0500

    Triangle only herons now

--- Lab1.2/src/shapes/Triangle.java
+++ Lab1.2/src/shapes/Triangle.java
@@ -3,6 +3,45 @@ package shapes;
 public class Triangle implements Shape
 
 {
+	private int side1;
+	private int side2;
+	private int side3;
+
+	public Triangle(int side1, int side2, int side3) 
+	{
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	public double calculateArea() 
+	{
+		//Heron's Formula		
+		double her = (calculatePerimeter()/2 * (((calculatePerimeter()/2) * side1) 
+				* ((calculatePerimeter()/2) * side2 ) * ((calculatePerimeter()/2) * side3))); 
+		double done = (Math.pow(her, 0.5));
+		return done;
+	}
+
+	public double calculatePerimeter() 
+	{
+		// To be written by student
+		return (side1 + side2 + side3);
+	}
+
+	public String toString() 
+	{
+		return "Triangle- \n" +  "   Side 1: " + side1 + "   Side 2: " + side2 + "   Side 3: " + side3 + "   Area(Heron's Forumala Given All Sides): " 
+		+ this.calculateArea() + " Perimeter: " + this.calculatePerimeter();
+	}
+
+}
+
+/* package shapes;
+
+public class Triangle implements Shape
+
+{
 	private int base;
 	private int height;
 	private int side1;
@@ -47,4 +86,4 @@ public class Triangle implements Shape
 
 	}
 
-}
+} */
